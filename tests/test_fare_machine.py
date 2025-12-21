27f91db3464a1e4006d61b3cfd528994b5cd8eac
mininfd <mizuki05nfd@gmail.com>
2025-12-21 11:05:06 +0900

    test(RED): 不足額0以下は0

--- tests/test_fare_machine.py
+++ tests/test_fare_machine.py
@@ -7,3 +7,15 @@ def test_start_reads_entry_and_balance_and_gets_fare():
   m = FareMachine(fares)
   m.start(card)
   assert m.get_shortage() == 80
+
+def test_shortage_below_zero_means_zero():
+  fares = {"A": 180}
+  card1 = ICCard(entry_station="A", balance=180)
+  m1 = FareMachine(fares)
+  m1.start(card1)
+  assert m1.get_shortage() == 0
+
+  card2 = ICCard(entry_station="A", balance=300)
+  m2 = FareMachine(fares)
+  m2.start(card2)
+  assert m2.get_shortage() == 0
